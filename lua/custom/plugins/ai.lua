d7c05c0992afa6c27077a68aa55570a687f250b0
keewee <kinycx@gmail.com>
2026-02-18 19:42:33 +0100

    chore: Replace copilot suggestion acception commands

--- lua/custom/plugins/ai.lua
+++ lua/custom/plugins/ai.lua
@@ -5,7 +5,15 @@ return {
     cmd = 'Copilot',
     event = 'InsertEnter',
     opts = {
-      suggestion = { enabled = true, auto_trigger = true },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = '<M-y>',
+          next = '<M-n>',
+          prev = '<M-p>',
+        },
+      },
     },
     keys = {
       { '<leader>ct', '<cmd>Copilot toggle<CR>', desc = 'Copilot [T]oggle' },
